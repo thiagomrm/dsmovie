e3f04ff0fe6f4fde4713851e0a64c1c8df61a8eb
thiagomrm <93530961+thiagomrm@users.noreply.github.com>
2022-03-12 16:32:16 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepositoty.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmovie.entities.User;
+
+public interface UserRepositoty extends JpaRepository<User, Long>{
+
+}
